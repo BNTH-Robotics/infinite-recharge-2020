bb5cc2b26fef6e6b1e091cf733d2a154e65c0be7
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-23 16:31:40 -0600

    Remove unused libraries

--- InfiniteChargez/src/main/include/Robot.h
+++ InfiniteChargez/src/main/include/Robot.h
@@ -6,7 +6,6 @@
 /*----------------------------------------------------------------------------*/
 #ifndef YEET_LE_MOST_AMAZING_ROBOT_IN_THE_WORLD
 #define YEET_LE_MOST_AMAZING_ROBOT_IN_THE_WORLD
- #pragma once
 
 #include "ControlCheckExec.h"
 #include "XboxInputHandler.h"
@@ -20,23 +19,21 @@
 #include "InputRecordAndPlay.h"
 #include "HandlesChecksAndExecs.h"
 
-#include <string>
-#include <frc/kinematics/DifferentialDriveOdometry.h>
-#include <frc/geometry/Rotation2d.h>
-#include <frc/geometry/Pose2d.h>
-#include <frc/Joystick.h>
-#include <frc/SpeedControllerGroup.h>
-#include <frc/TimedRobot.h>
-#include <frc/smartdashboard/SendableChooser.h>
-#include <frc/XboxController.h>
-#include <frc/drive/DifferentialDrive.h>
-#include <frc/BuiltInAccelerometer.h>
-#include <frc/geometry/Pose2d.h>
-#include <ctre/phoenix/motorcontrol/can/WPI_VictorSPX.h>
-
 #include <adi/ADIS16448_IMU.h>
 
+#include <ctre/phoenix/motorcontrol/can/WPI_VictorSPX.h>
+
+#include <frc/BuiltInAccelerometer.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/TimedRobot.h>
+#include <frc/SpeedControllerGroup.h>
+#include <frc/smartdashboard/SendableChooser.h>
+#include <frc/XboxController.h>
+
+
 #include <fstream>
+#include <string>
 
 class Robot : public frc::TimedRobot, public utilities::HandlesChecksAndExecs
 {
@@ -46,7 +43,6 @@ private:
   std::ifstream m_recordReadFile{};
   bool tankMode{false};
   //Configuration constants will go here until a configuration system can be set up
-  using joystick_t = frc::Joystick;
   using driveMotor_t = ctre::phoenix::motorcontrol::can::WPI_VictorSPX;
   using intakeMotor_t = driveMotor_t;
   using storageMotor_t = driveMotor_t;
@@ -63,22 +59,8 @@ private:
   using timePoint_t = std::chrono::steady_clock::time_point;
   using duration_t = std::chrono::duration<double>;
 
-  using driver_t = frc::DifferentialDrive;
   using handler_t = utilities::XboxInputHandler;
   private:
-   static constexpr double intakeSpeed{1};
-   static constexpr double speedMultiplier{0.75};
-   static constexpr double rotationMultiplier{1};
-    
-    /*The input is raised to a power to enable more
-    time on the lower curves
-    */
-
-    //Must be odd for the moment or else the robo cannot move backwards.
-   static constexpr double speedCurvePower{3};
-   static constexpr double rotationCurvePower{1};
-
-   //Automation Con
   //Ports for Motors and Controllers
  private:
      static constexpr int controllerPort{0};
@@ -120,7 +102,6 @@ private:
     RoboHook leHook{hookMotor};
 
      controller_t leController{controllerPort}; //Of epic dankness
-     joystick_t leJoystickLeft{leJoystickLeftPort};
      handler_t leInputHandler{};
   //Declare Motors
      driveMotor_t driveMotorFrontLeft{portDriveFrontLeft};
@@ -158,16 +139,10 @@ private:
     void intakeIn();
     void intakeOut();
     void intakeStop();
-    void updatePos(duration_t delta);
-  //Control handling nested class
-  //Declare Controllers
   //Declare Time Variables
-  timePoint_t lastSnapshot;
   frc::SendableChooser<std::string> m_chooser;
   const std::string kAutoNameDefault = "Yeeter McYeeterson";
   const std::string kAutoNameCustom = "Yeeter McYeeterson";
   std::string m_autoSelected;
 };
-
-void executeRecording(Robot *robot);
 #endif
