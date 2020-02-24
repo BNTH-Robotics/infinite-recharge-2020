857eec13625974937548bab0b44b1dd47edf6fc5
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-02-24 16:21:36 -0500

    Change player parser to new format

--- InfiniteChargez/src/main/cpp/Utilities/InputPlayer.cpp
+++ InfiniteChargez/src/main/cpp/Utilities/InputPlayer.cpp
@@ -15,28 +15,28 @@ namespace utilities
     void InputPlayer::playToAndExec(HandlesChecksAndExecs &probablyARobot)
     {
 
-        std::string delta{""};
-        std::getline(*m_recordingFile, delta);
+        //std::string delta{""};
+        //std::getline(*m_recordingFile, delta);
 
-        duration_t deltaTime{std::stod(delta)};
+        //duration_t deltaTime{std::stod(delta)};
         
 
 
-        m_recordingThread = std::thread{&InputPlayer::playRecordingExec, this, &probablyARobot, deltaTime};
+        m_recordingThread = std::thread{&InputPlayer::playRecordingExec, this, &probablyARobot};
         m_recordingThread.detach();
 
     }
     
-    void InputPlayer::playRecordingExec(HandlesChecksAndExecs *probablyARobot, duration_t delta)
-    {
-        std::string snapshot{""};
-        while(std::getline(*m_recordingFile, snapshot))
-        {
-            probablyARobot->getInputHandler() = snapshot;
-            probablyARobot->checkAndExec();
-            std::this_thread::sleep_for(delta);
-        }
-    }
+    //void InputPlayer::playRecordingExec(HandlesChecksAndExecs *probablyARobot, duration_t delta)
+    //{
+    //    std::string snapshot{""};
+    //    while(std::getline(*m_recordingFile, snapshot))
+    //    {
+    //        probablyARobot->getInputHandler() = snapshot;
+    //        probablyARobot->checkAndExec();
+    //        std::this_thread::sleep_for(delta);
+    //    }
+    //}
 
     void InputPlayer::playRecordingExec(HandlesChecksAndExecs *probablyARobot)
     {
