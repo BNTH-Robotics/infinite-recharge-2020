dedc1e7d25165114ddefe18d16a2443e31c08e21
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-07 11:29:11 -0600

    Add debug messages

--- InfiniteChargez/src/main/cpp/Robot.cpp
+++ InfiniteChargez/src/main/cpp/Robot.cpp
@@ -146,9 +146,7 @@ void Robot::AutonomousPeriodic()
 
 void Robot::TeleopInit()
 {
-  std::cout << m_chooser.GetSelected() << '\n';
-  m_leRecordScribe.stopLoadedRecording();
-  m_recordFile.open(inputRecordFileName + m_chooser.GetSelected());
+  
   leRoboData.initSnap();
 }
 
