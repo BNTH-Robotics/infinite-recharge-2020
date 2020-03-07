cfbef0d0a0d9bae030d61180aa9c9b766b9c0043
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-07 08:26:56 -0600

    Move joystick code to decision tree

--- InfiniteChargez/src/main/cpp/controls/ControlCheckExec.cpp
+++ InfiniteChargez/src/main/cpp/controls/ControlCheckExec.cpp
@@ -57,32 +57,30 @@
             {
                 leStorage.tankHalt();
             }
+            //Joystick Block Code
+            {
+                ////Compilar arguments and preprocessor macros can be passed to remove unused
+                //const double JoystickLeftX{leInputHandler.getJoystickLeft().x};
+                ////Up is negative in Xbox controllers
+                //const double JoystickLeftY{-leInputHandler.getJoystickLeft().y};
+
+                //const double JoystickRightX{joystickLeft.x};
+                ////Up isnegative in Xbox controllers
+                //const double JoystickRightY{-joystickLeft.y};
+        
+                ////Squarified Values
+                //const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
+                //const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
+                utilities::Pair2D<double> joystickValuesThatMakeSense{-leInputHandler.getJoystickLeft().y, -leInputHandler.getJoystickLeft().x};
+                leInputHandler.setJoystickLeft(joystickValuesThatMakeSense); //Computer scientists are stupid. Up is down ahhhhh.
+
+                leDrive.setMovementMap(leInputHandler.getJoystickLeft());
+            }
         }
         
 
         std::cout << leInputHandler.getJoystickLeft().x << '\n';
-        joystickPosition(leInputHandler.getJoystickLeft(), leInputHandler.getJoystickRight());
     }
     
-    void Robot::joystickPosition(utilities::XboxInputHandler::joystick_t &&joystickLeft, utilities::XboxInputHandler::joystick_t &&joystickRight)
-    {
-        ////Compilar arguments and preprocessor macros can be passed to remove unused
-        //const double JoystickLeftX{leInputHandler.getJoystickLeft().x};
-        ////Up is negative in Xbox controllers
-        //const double JoystickLeftY{-leInputHandler.getJoystickLeft().y};
-
-        //const double JoystickRightX{joystickLeft.x};
-        ////Up isnegative in Xbox controllers
-        //const double JoystickRightY{-joystickLeft.y};
-        
-        ////Squarified Values
-        //const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
-        //const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
-        utilities::Pair2D<double> joystickValuesThatMakeSense{-leInputHandler.getJoystickLeft().y, -leInputHandler.getJoystickLeft().x};
-        leInputHandler.setJoystickLeft(joystickValuesThatMakeSense); //Computer scientists are stupid. Up is down ahhhhh.
-
-        leDrive.setMovementMap(leInputHandler.getJoystickLeft());
-    }
-
-
+    
 
