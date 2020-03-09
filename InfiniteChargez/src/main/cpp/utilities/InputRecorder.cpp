0acf983bd8e9a014dacb40dcc070b5705b363c07
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-09 00:00:51 -0500

    rename utilities

---
+++ InfiniteChargez/src/main/cpp/utilities/InputRecorder.cpp
@@ -0,0 +1,56 @@
+#include "InputRecorder.h"
+
+#include <sstream>
+#include <string>
+#include <thread>
+
+#include <iostream>
+namespace utilities
+{
+    void InputRecorder::designateFile(std::ofstream &recordingFile)
+    {
+        m_recording = &recordingFile;
+    }
+
+    void InputRecorder::recordFrom(HandlesChecksAndExecs &probablyARobot)
+    {
+    }
+
+    void InputRecorder::snapFrom(HandlesChecksAndExecs *probablyARobot)
+    {
+        m_recordingBuffer << probablyARobot->getInputHandler().getSnapshot() << '\n';
+    }
+
+    void InputRecorder::snapFrom(HandlesChecksAndExecs *probablyARobot, std::chrono::duration<double> delta)
+    {
+        m_recordingBuffer << delta.count() << ' ' << probablyARobot->getInputHandler().getSnapshot() << '\n';
+    }
+
+    void InputRecorder::stopRecording(std::chrono::duration<double> delta)
+    {
+        std::string line{""};
+
+        *m_recording << delta.count() << '\n';
+
+    while (std::getline(m_recordingBuffer, line))
+        {
+            *m_recording << line << '\n';
+        }
+        
+      std::cout << "DONE RECORDING\n";
+    }
+    void InputRecorder::stopRecording()
+    {
+        std::string line{""};
+
+    while (std::getline(m_recordingBuffer, line))
+        {
+            std::cout << line << '\n';
+            *m_recording << line << '\n';
+        }
+        
+      std::cout << "DONE RECORDING\n";
+    }
+
+
+}
