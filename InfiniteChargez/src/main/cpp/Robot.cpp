b6b5114b507166cfd61c53fb1a26ed4247cad97c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-12 23:27:27 -0500

    Add documentation comments to Robot.cpp

--- InfiniteChargez/src/main/cpp/Robot.cpp
+++ InfiniteChargez/src/main/cpp/Robot.cpp
@@ -23,12 +23,15 @@
 
 Robot::Robot() : frc::TimedRobot{10_ms}
 {
+    //Neutral Mode means the motor will give easily to external foces. Keeps the arm from flailing armound.
     hookMotor.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
     tankMotor.SetNeutralMode(ctre::phoenix::motorcontrol::NeutralMode::Brake);
 }
 
 void Robot::RobotInit()
 {
+    //Adds buttons to a "Sendable Chooser" which can be used to display options
+    //They represent recording files that can be chosen
     m_chooser.SetDefaultOption("Emergency-Forward and Stop", "Emergency");
     m_chooser.AddOption("1-DumpAndYeet", "R1-DumpAndYeet.rcd");
     m_chooser.AddOption("2-DumpAndYeet", "R2-DumpAndYeet.rcd");
@@ -37,11 +40,15 @@ void Robot::RobotInit()
     m_chooser.AddOption("1-CrossTheLine", "R1-CrossTheLine.rcd");
     m_chooser.AddOption("2-CrossTheLine", "R2-CrossTheLine.rcd");
     m_chooser.AddOption("3-CrossTheLine", "R3-CrossTheLine.rcd");
+
+    //Emergency Mode is hard-coded
     m_chooser.AddOption("Emergency-Forward and Stop", "Emergency");
 
+    //Could be removed if the motor ports for the drive motors are switched
     driveMotorsLeft.SetInverted(true);
     driveMotorsRight.SetInverted(true);
 
+    //Places the buttons the the Smart Dashboard
     frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 }
 
@@ -51,6 +58,7 @@ void Robot::RobotPeriodic()
 
 void Robot::AutonomousInit()
 {
+    //Prevents multiple recordings being played simultaneously because that would be chaos
     m_leRecordScribe.stopLoadedRecording();
     std::string chosen{m_chooser.GetSelected()};
     if (chosen == "Emergency")
@@ -61,7 +69,9 @@ void Robot::AutonomousInit()
     }
     else
     {
+        
         std::cout << m_chooser.GetSelected() << '\n';
+        //Makes sure that no file is open because if a file is already open, the new recording will not load
         if (m_recordReadFile.is_open())
         {
             std::cout << "Closed Open File \n";
@@ -72,6 +82,7 @@ void Robot::AutonomousInit()
         m_recordReadFile.seekg(0, std::ios::beg);
         assert(m_recordReadFile.is_open());
 
+        //Play recording
         m_leRecordScribe.loadRecording(m_recordReadFile);
         m_leRecordScribe.playLoadedRecordingToAndExec(this);
 
@@ -85,7 +96,7 @@ void Robot::AutonomousPeriodic()
 
 void Robot::TeleopInit()
 {
-
+    //Make sure no recording is playing during teleop as the controller and the playback thread would fight over the robot *spooky*
     m_leRecordScribe.stopLoadedRecording();
     leRoboData.initSnap();
 }
