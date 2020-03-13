2faa1295b1d15beeee56eba1979f5abf56c971ac
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-12 23:58:57 -0500

    Fix RoboStorage Indentation

--- InfiniteChargez/src/main/include/RobotComponents/RoboStorage.h
+++ InfiniteChargez/src/main/include/RobotComponents/RoboStorage.h
@@ -7,54 +7,54 @@
 
 class RoboStorage
 {
-public:
-    RoboStorage(frc::SpeedController &intakeMotor, frc::SpeedController &tankMotor):
-      m_intakeMotor{&intakeMotor}, m_tankMotor{&tankMotor} 
-      {
-      }
-    void setIntakeSpeed()
-    {}
-    void raise();
-    void lower();
-    void intakeIn(double slowdown = 0);
-    void intakeEject(double slowdown = 0);
-    void intakeStop();
-    void tankHalt();
+    public:
+        RoboStorage(frc::SpeedController &intakeMotor, frc::SpeedController &tankMotor):
+            m_intakeMotor{&intakeMotor}, m_tankMotor{&tankMotor} 
+        {
+        }
+        void setIntakeSpeed()
+        {}
+        void raise();
+        void lower();
+        void intakeIn(double slowdown = 0);
+        void intakeEject(double slowdown = 0);
+        void intakeStop();
+        void tankHalt();
 
-    int getIntakeStatus() 
-    {
-      if(m_intakeMotor->Get() > 0)
-      {
-        return 1;
-      } 
-      else if (m_intakeMotor->Get() < 0)
-      {
-        return -1;
-      }
-      else
-      {
-        return 0;
-      }
-    }
-    void tankHandler();
-    //void setAngle();
+        int getIntakeStatus() 
+        {
+            if(m_intakeMotor->Get() > 0)
+            {
+                return 1;
+            } 
+            else if (m_intakeMotor->Get() < 0)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        void tankHandler();
+        //void setAngle();
 
-    void setTankMotorManual(double vel);
-private:
-  frc::SpeedController* m_intakeMotor;
-  frc::SpeedController* m_tankMotor;
-  //Basic motor numbers
-    static constexpr double tankRaiseSpeed{0.75};
-    static constexpr double tankLowerSpeed{0.2};
-    static constexpr double intakeInSpeed{0.7};
-    static constexpr double intakeEjectSpeed{-0.7};
+        void setTankMotorManual(double vel);
+    private:
+        frc::SpeedController* m_intakeMotor;
+        frc::SpeedController* m_tankMotor;
+        //Basic motor numbers
+        static constexpr double tankRaiseSpeed{0.75};
+        static constexpr double tankLowerSpeed{0.2};
+        static constexpr double intakeInSpeed{0.7};
+        static constexpr double intakeEjectSpeed{-0.7};
 
-    //Tank Handler values(unused)
-    static constexpr double tankToggleUpSpeed{.6};
-    static constexpr double tankToggleDownSpeed{-1.0};
-    static constexpr double tankRaiseStop{-0.2};
-    static constexpr double changeRate{0.012};
-    bool manual{false};
-    bool m_raising{true};
+        //Tank Handler values(unused)
+        static constexpr double tankToggleUpSpeed{.6};
+        static constexpr double tankToggleDownSpeed{-1.0};
+        static constexpr double tankRaiseStop{-0.2};
+        static constexpr double changeRate{0.012};
+        bool manual{false};
+        bool m_raising{true};
 };
 #endif
