fee4270acab82d90bfca2b4d0305b9ed33eea9b3
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-12 23:50:01 -0500

    Spacing cleanup

--- InfiniteChargez/src/main/cpp/controls/Recording.cpp
+++ InfiniteChargez/src/main/cpp/controls/Recording.cpp
@@ -35,10 +35,12 @@ void Robot::recordingInit()
     recordingEnabled = false;
     std::cout << m_chooser.GetSelected() << '\n';
     m_leRecordScribe.stopLoadedRecording();
+
     if (m_recordFile.is_open())
     {
         m_recordFile.close();
     }
+
     m_recordFile.clear();
     m_recordFile.open(inputRecordFileName + m_chooser.GetSelected());
     m_leRecordScribe.designateFile(m_recordFile);
