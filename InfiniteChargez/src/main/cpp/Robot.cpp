38abbd30a28eee888bd43eccde67ff74fd20a99c
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-12 23:08:23 -0500

    Remove unused comment code from Robot.cpp

--- InfiniteChargez/src/main/cpp/Robot.cpp
+++ InfiniteChargez/src/main/cpp/Robot.cpp
@@ -38,7 +38,6 @@ Robot::Robot() : frc::TimedRobot{10_ms}
 
 void Robot::RobotInit()
 {
-  //leStorage.startHandler();
   m_chooser.SetDefaultOption("Emergency-Forward and Stop", "Emergency");
   m_chooser.AddOption("1-DumpAndYeet", "R1-DumpAndYeet.rcd");
   m_chooser.AddOption("2-DumpAndYeet", "R2-DumpAndYeet.rcd");
@@ -49,25 +48,11 @@ void Robot::RobotInit()
   m_chooser.AddOption("3-CrossTheLine", "R3-CrossTheLine.rcd");
   m_chooser.AddOption("Emergency-Forward and Stop", "Emergency");
 
-  //m_chooser.AddOption("R1", "R1.rcd");
-  //m_chooser.AddOption("R2", "R2.rcd");
-  //m_chooser.AddOption("R3", "R3.rcd");
-  //m_chooser.AddOption("B1", "B1.rcd");
-  //m_chooser.AddOption("B2", "B2.rcd");
-  //m_chooser.AddOption("B3", "B3.rcd");
   driveMotorsLeft.SetInverted(true);
   driveMotorsRight.SetInverted(true);
 
-
-
-
-
-
-
-
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
 }
-
 
 void Robot::RobotPeriodic()
 {
@@ -102,31 +87,11 @@ void Robot::AutonomousInit()
   m_leRecordScribe.playLoadedRecordingToAndExec(this);
 
   m_autoSelected = m_chooser.GetSelected();
-  // m_autoSelected = SmartDashboard::GetString("Auto Selector",
-  //     kAutoNameDefault);
-  //std::cout << "Auto selected: " << m_autoSelected << std::endl;
-
-  //if (m_autoSelected == kAutoNameCustom)
-  //{
-  //  // Custom Auto goes here
-  //}
-  //else
-  //{
-  //  // Default Auto goes here
-  //}
   }
 }
 
 void Robot::AutonomousPeriodic()
 {
-  if (m_autoSelected == kAutoNameCustom)
-  {
-    // Custom Auto goes here
-  }
-  else
-  {
-    // Default Auto goes here
-  }
 }
 
 void Robot::TeleopInit()
