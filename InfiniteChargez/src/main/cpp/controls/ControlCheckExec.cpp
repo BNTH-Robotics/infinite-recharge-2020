b12f96200379e75d71282b47441c6fd4b5625765
RavenJFelix <58748161+RavenJFelix@users.noreply.github.com>
2020-03-12 22:53:10 -0500

    Fix indentation

--- InfiniteChargez/src/main/cpp/controls/ControlCheckExec.cpp
+++ InfiniteChargez/src/main/cpp/controls/ControlCheckExec.cpp
@@ -5,34 +5,34 @@
 #include <iostream>
 #include <math.h>
 
-    void Robot::checkAndExec()
+void Robot::checkAndExec()
+{
+    //Output tree X will switch the controlls to "Hook Mode"
+    if (leInputHandler.getButtonXState())
     {
-        //Output tree X will switch the controlls to "Hook Mode"
-        if (leInputHandler.getButtonXState())
-        {
-            hookModeExec();
-        }
-        else if (leInputHandler.getTriggerLeft() > triggerIntakeTolerance)
-        {
-           manualTankExec(); 
-        }
-        else
-        {
-            regularExec();
-        }
+        hookModeExec();
     }
-    
+    else if (leInputHandler.getTriggerLeft() > triggerIntakeTolerance)
+    {
+        manualTankExec();
+    }
+    else
+    {
+        regularExec();
+    }
+}
+
 void Robot::hookModeExec()
 {
-if(leInputHandler.getBumperLeftState())
-            {
-                std::cout << "Hook advance \n";
-                leHook.advance();
-            }
-            else
-            {
-                leHook.stop();
-            }
+    if (leInputHandler.getBumperLeftState())
+    {
+        std::cout << "Hook advance \n";
+        leHook.advance();
+    }
+    else
+    {
+        leHook.stop();
+    }
 }
 
 void Robot::manualTankExec()
@@ -43,53 +43,50 @@ void Robot::manualTankExec()
 
 void Robot::regularExec()
 {
-if (leInputHandler.getBumperLeftState())
-            {
-                //if(!(leStorage.getIntakeStatus() == -1))
-                //{
-                leStorage.intakeIn(leInputHandler.getTriggerRight());
-                //}
-                //else if (leStorage.getIntakeStatus() == -1)
-                //{
-                //leStorage.intakeStop();
-                //}
+    if (leInputHandler.getBumperLeftState())
+    {
+        //if(!(leStorage.getIntakeStatus() == -1))
+        //{
+        leStorage.intakeIn(leInputHandler.getTriggerRight());
+        //}
+        //else if (leStorage.getIntakeStatus() == -1)
+        //{
+        //leStorage.intakeStop();
+        //}
+    }
+    else if (leInputHandler.getBumperRightState())
+    {
+        leStorage.intakeEject(leInputHandler.getTriggerRight());
+    }
+    if (leInputHandler.getButtonAState())
+    {
+        leStorage.lower();
+    }
+    else if (leInputHandler.getButtonBState())
+    {
+        leStorage.raise();
+    }
+    else
+    {
+        leStorage.tankHalt();
+    }
+    //Joystick Block Code
+    {
+        ////Compilar arguments and preprocessor macros can be passed to remove unused
+        //const double JoystickLeftX{leInputHandler.getJoystickLeft().x};
+        ////Up is negative in Xbox controllers
+        //const double JoystickLeftY{-leInputHandler.getJoystickLeft().y};
 
-            }
-            else if (leInputHandler.getBumperRightState())
-            {
-                leStorage.intakeEject(leInputHandler.getTriggerRight());
-            }
-            if (leInputHandler.getButtonAState())
-            {
-                leStorage.lower();
-            }
-            else if (leInputHandler.getButtonBState())
-            {
-                leStorage.raise();
-            }
-            else
-            {
-                leStorage.tankHalt();
-            }
-            //Joystick Block Code
-            {
-                ////Compilar arguments and preprocessor macros can be passed to remove unused
-                //const double JoystickLeftX{leInputHandler.getJoystickLeft().x};
-                ////Up is negative in Xbox controllers
-                //const double JoystickLeftY{-leInputHandler.getJoystickLeft().y};
+        //const double JoystickRightX{joystickLeft.x};
+        ////Up isnegative in Xbox controllers
+        //const double JoystickRightY{-joystickLeft.y};
 
-                //const double JoystickRightX{joystickLeft.x};
-                ////Up isnegative in Xbox controllers
-                //const double JoystickRightY{-joystickLeft.y};
-        
-                ////Squarified Values
-                //const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
-                //const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
-                utilities::Pair2D<double> joystickValuesThatMakeSense{-leInputHandler.getJoystickLeft().y, -leInputHandler.getJoystickLeft().x};
-                leInputHandler.setJoystickLeft(joystickValuesThatMakeSense); //Computer scientists are stupid. Up is down ahhhhh.
+        ////Squarified Values
+        //const utilities::Pair2D<double> SquareJoystickLeft{utilities::squarify(JoystickLeftX, JoystickLeftY)};
+        //const utilities::Pair2D<double> SquareJoystickRight{utilities::squarify(JoystickRightX, JoystickRightY)};
+        utilities::Pair2D<double> joystickValuesThatMakeSense{-leInputHandler.getJoystickLeft().y, -leInputHandler.getJoystickLeft().x};
+        leInputHandler.setJoystickLeft(joystickValuesThatMakeSense); //Computer scientists are stupid. Up is down ahhhhh.
 
-                leDrive.setMovementMap(leInputHandler.getJoystickLeft());
-            }
+        leDrive.setMovementMap(leInputHandler.getJoystickLeft());
+    }
 }
-    
-
